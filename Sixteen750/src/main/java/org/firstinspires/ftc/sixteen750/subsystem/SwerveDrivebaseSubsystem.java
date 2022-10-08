4845437bdf0c0812d64e96bb7c5e29dec781b170
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-10-08 12:43:35 -0700

    eliminated more error in SwerveDrivebaseSubsystem.java without adding more error elsewhere

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/subsystem/SwerveDrivebaseSubsystem.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/subsystem/SwerveDrivebaseSubsystem.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.sixteen750.subsystem;
 import androidx.annotation.GuardedBy;
 import androidx.annotation.NonNull;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.control.PIDCoefficients;
 import com.acmerobotics.roadrunner.drive.DriveSignal;
 import com.acmerobotics.roadrunner.drive.SwerveDrive;
@@ -71,11 +72,80 @@ public class SwerveDrivebaseSubsystem extends SwerveDrive {
     @GuardedBy("IMULock")
     private BNO055IMU imu;
 
-    public SwerveDrivebaseSubsystem(HardwareMap hardwareMap) {
-        super(kV, kA, kStatic, TRACK_WIDTH);
+    @Config
+    public static class DriveConstant {
+        /*
+         * These are motor constants that should be listed online for your motors.
+         */
+        public static final double TICKS_PER_REV = 28;
+        public static final double MAX_RPM = 6000;
 
-        velocityConstraint = getVelocityConstraint(MAX_VEL, MAX_ANG_VEL, TRACK_WIDTH);
-        accelConstraint = getAccelerationConstraint(MAX_ACCEL);
+        /*
+         * Set RUN_USING_ENCODER to true to enable built-in hub velocity control using drive encoders.
+         * Set this flag to false if drive encoders are not present and an alternative localization
+         * method is in use (e.g., tracking wheels).
+         *
+         * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
+         * from DriveVelocityPIDTuner.
+         */
+        public static final boolean RUN_USING_ENCODER = true;
+        public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(30, 0, 8,
+                getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+
+        /*
+         * These are physical constants that can be determined from your robot (including the track
+         * width; it will be tune empirically later although a rough estimate is important). Users are
+         * free to chose whichever linear distance unit they would like so long as it is consistently
+         * used. The default values were selected with inches in mind. Road runner uses radians for
+         * angular distances although most angular parameters are wrapped in Math.toRadians() for
+         * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
+         */
+        public static double WHEEL_RADIUS = 1.4; // in
+        public static double GEAR_RATIO = 1/(3.5*1.5*2); // output (wheel) speed / input (motor) speed
+        public static double TRACK_WIDTH = 9; // in
+
+        /*
+         * These are the feedforward parameters used to model the drive motor behavior. If you are using
+         * the built-in velocity PID, *these values are fine as is*. However, if you do not have drive
+         * motor encoders or have elected not to use them for velocity control, these values should be
+         * empirically tuned.
+         */
+        public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
+        public static double kA = 0;
+        public static double kStatic = 0;
+
+        /*
+         * These values are used to generate the trajectories for you robot. To ensure proper operation,
+         * the constraints should never exceed ~80% of the robot's actual capabilities. While Road
+         * Runner is designed to enable faster autonomous motion, it is a good idea for testing to start
+         * small and gradually increase them later after everything is working. All distance units are
+         * inches.
+         */
+        public static double MAX_VEL = 60;
+        public static double MAX_ACCEL = 60;
+        public static double MAX_ANG_VEL = Math.toRadians(120);
+        public static double MAX_ANG_ACCEL = Math.toRadians(120);
+
+
+        public static double encoderTicksToInches(double ticks) {
+            return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV;
+        }
+
+        public static double rpmToVelocity(double rpm) {
+            return rpm * GEAR_RATIO * 2 * Math.PI * WHEEL_RADIUS / 60.0;
+        }
+
+        public static double getMotorVelocityF(double ticksPerSecond) {
+            // see https://docs.google.com/document/d/1tyWrXDfMidwYyP_5H4mZyVgaEswhOC35gvdmP-V-5hA/edit#heading=h.61g9ixenznbx
+            return 32767 / ticksPerSecond;
+        }
+    }
+
+    public SwerveDrivebaseSubsystem(HardwareMap hardwareMap) {
+        super(DriveConstant.kV, DriveConstant.kA, DriveConstantkStatic, DriveConstant.TRACK_WIDTH);
+
+        velocityConstraint = getVelocityConstraint(DriveConstant.MAX_VEL, DriveConstant.MAX_ANG_VEL, DriveConstant.TRACK_WIDTH);
+        accelConstraint = getAccelerationConstraint(DriveConstant.MAX_ACCEL);
 
         follower = new HolonomicPIDVAFollower(TRANSLATIONAL_PID, TRANSLATIONAL_PID, HEADING_PID,
                 new Pose2d(0.5, 0.5, Math.toRadians(5)), 0);
@@ -123,7 +193,7 @@ public class SwerveDrivebaseSubsystem extends SwerveDrive {
         modules = Arrays.asList(leftFrontModule, leftRearModule, rightRearModule, rightFrontModule);
 
 
-        if (RUN_USING_ENCODER) {
+        if (DriveConstant.RUN_USING_ENCODER) {
             setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
 
@@ -133,8 +203,8 @@ public class SwerveDrivebaseSubsystem extends SwerveDrive {
         //TODO instantiate hardware
 
 
-        if (RUN_USING_ENCODER && MOTOR_VELO_PID != null) {
-            setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, MOTOR_VELO_PID);
+        if (DriveConstant.RUN_USING_ENCODER && DriveConstant.MOTOR_VELO_PID != null) {
+            setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, DriveConstant.MOTOR_VELO_PID);
         }
 
         // TODO: reverse any motors using DcMotor.setDirection()
@@ -180,16 +250,16 @@ public class SwerveDrivebaseSubsystem extends SwerveDrive {
     public static TrajectorySequenceBuilder trajectorySequenceBuilder(Pose2d startPose) {
         return new TrajectorySequenceBuilder(
                 startPose,
-                getVelocityConstraint(MAX_VEL, MAX_ANG_VEL, TRACK_WIDTH), getAccelerationConstraint(MAX_ACCEL),
-                MAX_ANG_VEL, MAX_ANG_ACCEL
+                getVelocityConstraint(DriveConstant.MAX_VEL, DriveConstant.MAX_ANG_VEL, DriveConstant.TRACK_WIDTH), getAccelerationConstraint(DriveConstant.MAX_ACCEL),
+                DriveConstant.MAX_ANG_VEL, DriveConstant.MAX_ANG_ACCEL
         );
     }
     public static TrajectorySequenceBuilder trajectorySequenceBuilder(Pose2d startPose, double startHeading) {
         return new TrajectorySequenceBuilder(
                 startPose,
                 startHeading,
-                getVelocityConstraint(MAX_VEL, MAX_ANG_VEL, TRACK_WIDTH), getAccelerationConstraint(MAX_ACCEL),
-                MAX_ANG_VEL, MAX_ANG_ACCEL
+                getVelocityConstraint(DriveConstant.MAX_VEL, DriveConstant.MAX_ANG_VEL, DriveConstant.TRACK_WIDTH), getAccelerationConstraint(DriveConstant.MAX_ACCEL),
+                DriveConstant.MAX_ANG_VEL, DriveConstant.MAX_ANG_ACCEL
         );
     }
     public void turnAsync(double angle) {
