f30cd747c0613be67470cb1f47e904ba4d562a1a
Kevin Frei <kevinfrei@hotmail.com>
2022-10-08 08:02:49 -0700

    Format the code

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/command/autonomous/AutoConstantsRed.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/command/autonomous/AutoConstantsRed.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.sixteen750.command.autonomous;
 
-
 import static java.lang.Math.toRadians;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
@@ -8,14 +7,14 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 public class AutoConstantsRed {
     // Home
     public static Pose2d HOME_START = new Pose2d(36, -66, toRadians(-90));
-    public static Pose2d HOME_JUNCTION = new Pose2d(28,4,toRadians(-135));
-    public static Pose2d HOME_STACK = new Pose2d(62,12, toRadians(0));
-    public static Pose2d HOME_LEFT = new Pose2d(60,36, toRadians(180));
+    public static Pose2d HOME_JUNCTION = new Pose2d(28, 4, toRadians(-135));
+    public static Pose2d HOME_STACK = new Pose2d(62, 12, toRadians(0));
+    public static Pose2d HOME_LEFT = new Pose2d(60, 36, toRadians(180));
     public static Pose2d HOME_MIDDLE = new Pose2d(36, 36, toRadians(90));
     public static Pose2d HOME_RIGHT = new Pose2d(12, 36, toRadians(90));
 
-    //Away
-    public static Pose2d AWAY_START = new Pose2d(-36, 66,toRadians(-90));
+    // Away
+    public static Pose2d AWAY_START = new Pose2d(-36, 66, toRadians(-90));
     public static Pose2d AWAY_JUNCTION = new Pose2d(-28, 4, toRadians(-45));
     public static Pose2d AWAY_STACK = new Pose2d(-62, 12, toRadians(180));
     public static Pose2d AWAY_LEFT = new Pose2d(-12, 36, toRadians(90));
