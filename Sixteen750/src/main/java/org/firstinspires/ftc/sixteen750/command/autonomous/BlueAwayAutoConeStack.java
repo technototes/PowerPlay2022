55dcd1c8600ea637965c2682f866cb86e994f13d
Yinn333 <hoda.ata18@gmail.com>
2022-10-07 17:51:14 -0700

    Added coordinates to all junctions besides center, merged, and added trajectories

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/command/autonomous/BlueAwayAutoConeStack.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/command/autonomous/BlueAwayAutoConeStack.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.sixteen750.command.autonomous;
 
 import org.firstinspires.ftc.sixteen750.Robot;
+import org.firstinspires.ftc.sixteen750.command.claw.ClawOpenCommand;
+import org.firstinspires.ftc.sixteen750.command.cone.ConeReadyToIntakeCommand;
+import org.firstinspires.ftc.sixteen750.command.cone.ConeReadyToScoreCommand;
 import org.firstinspires.ftc.sixteen750.subsystem.ClawSubsystem;
 import org.firstinspires.ftc.sixteen750.subsystem.ConeSubsystem;
 import org.firstinspires.ftc.sixteen750.subsystem.LiftSubsystem;
@@ -11,8 +14,8 @@ import com.technototes.path.subsystem.MecanumDrivebaseSubsystem;
 
 public class BlueAwayAutoConeStack extends SequentialCommandGroup {
     public BlueAwayAutoConeStack(MecanumDrivebaseSubsystem drive, ClawSubsystem claw, LiftSubsystem lift, ConeSubsystem cone) {
-        super (new TrajectorySequenceCommand(drive,RobotConstant.BLUE_AWAY_STACK).alongWith(new ConeReadyToIntakeCommand(cone)),
-                TrajectorySequenceCommand(drive, RobotConstant.HIGH_JUNCTION_AWAY).alongWith(new ConeReadyToScoreCommand(cone)),
+        super (new TrajectorySequenceCommand(drive,Robot.Trajectories.BLUE_AWAY_STACK).alongWith(new ConeReadyToIntakeCommand(cone)),
+                new TrajectorySequenceCommand(drive, Robot.Trajectories.BLUE_HIGH_JUNCTION_AWAY).alongWith(new ConeReadyToScoreCommand(cone)),
                 new ClawOpenCommand(claw) );
     }
 
--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/command/autonomous/BlueHomeConeStackCommand.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/command/autonomous/BlueHomeConeStackCommand.java
@@ -13,10 +13,10 @@ import com.technototes.path.subsystem.MecanumDrivebaseSubsystem;
 
 public class BlueHomeConeStackCommand extends SequentialCommandGroup {
     public BlueHomeConeStackCommand(MecanumDrivebaseSubsystem drive, ClawSubsystem claw, LiftSubsystem lift, ConeSubsystem cone){
-        super(new TrajectorySequenceCommand(drive, RobotConstant.HIGH_JUNCTION_HOME).alongWith(new ConeReadyToScoreCommand(cone)),
+        super(new TrajectorySequenceCommand(drive, Robot.Trajectories.BLUE_HIGH_JUNCTION_HOME).alongWith(new ConeReadyToScoreCommand(cone)),
                 new ClawOpenCommand(claw), new BlueHomeAutoConeStack(drive, claw, lift, cone), new BlueHomeAutoConeStack(drive, claw, lift, cone),
                 new BlueHomeAutoConeStack(drive, claw, lift, cone),new BlueHomeAutoConeStack(drive, claw, lift, cone),
-                new TrajectorySequenceCommand(drive.RobotConstant.PARK_LOCATION)/*Placeholder for what we're doing for parking*/);
+                new TrajectorySequenceCommand(drive, Robot.Trajectories.BLUE_PARK_LOCATION_HOME)/*Placeholder for what we're doing for parking*/);
 
     }
 }
