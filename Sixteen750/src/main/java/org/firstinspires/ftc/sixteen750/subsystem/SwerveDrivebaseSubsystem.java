edd1bbdf31333624119b92b925a2c64f7d44f3f3
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-10-12 16:39:12 -0700

    recover the comments on DriveConstant

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/subsystem/SwerveDrivebaseSubsystem.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/subsystem/SwerveDrivebaseSubsystem.java
@@ -74,6 +74,17 @@ public class SwerveDrivebaseSubsystem extends SwerveDrive {
     @GuardedBy("IMULock")
     private BNO055IMU imu;
 
+    /*
+     * Constants shared between multiple drive types.
+     *
+     * TODO: Tune or adjust the following constants to fit your robot. Note that the non-final
+     * fields may also be edited through the dashboard (connect to the robot's WiFi network and
+     * navigate to https://192.168.49.1:8080/dash). Make sure to save the values here after you
+     * adjust them in the dashboard; **config variable changes don't persist between app restarts**.
+     *
+     * These are not the only parameters; some are located in the localizer classes, drive base classes,
+     * and op modes themselves.
+     */
     @Config
     public static class DriveConstant {
         /*
