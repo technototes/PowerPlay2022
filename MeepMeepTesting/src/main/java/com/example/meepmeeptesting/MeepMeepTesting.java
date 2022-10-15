0fc7ad5ddf511d0e1d790e2f672a02dc8e75e69c
FTC16750 <technototes@outlook.com>
2022-10-14 17:41:07 -0700

    Changed coordinates of robot positions some more

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -5,6 +5,7 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(750);
@@ -14,6 +15,28 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 9.5)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                                .addTrajectory(AutoConstantsBlue.Away.START_TO_W_JUNCTION.get())
+                                .addTrajectory(AutoConstantsBlue.Away.W_JUNCTION_TO_BETWEEN_TO_STACK.get())
+                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_T0_STACK_TO_STACK.get())
+                                .addTrajectory(AutoConstantsBlue.Away.STACK_TO_BETWEEN_TO_JUNCTION.get())
+                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_TO_JUNCTION_TO_JUNCTION.get())
+                                .addTrajectory(AutoConstantsBlue.Away.W_JUNCTION_TO_BETWEEN_TO_STACK.get())
+                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_T0_STACK_TO_STACK.get())
+                                .addTrajectory(AutoConstantsBlue.Away.STACK_TO_BETWEEN_TO_JUNCTION.get())
+                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_TO_JUNCTION_TO_JUNCTION.get())
+                                .addTrajectory(AutoConstantsBlue.Away.W_JUNCTION_TO_BETWEEN_TO_STACK.get())
+                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_T0_STACK_TO_STACK.get())
+                                .addTrajectory(AutoConstantsBlue.Away.STACK_TO_BETWEEN_TO_JUNCTION.get())
+                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_TO_JUNCTION_TO_JUNCTION.get())
+                                .addTrajectory(AutoConstantsBlue.Away.W_JUNCTION_TO_BETWEEN_TO_STACK.get())
+                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_T0_STACK_TO_STACK.get())
+                                .addTrajectory(AutoConstantsBlue.Away.STACK_TO_BETWEEN_TO_JUNCTION.get())
+                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_TO_JUNCTION_TO_JUNCTION.get())
+                                .addTrajectory(AutoConstantsBlue.Away.W_JUNCTION_TO_BETWEEN_TO_STACK.get())
+                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_T0_STACK_TO_STACK.get())
+                                .addTrajectory(AutoConstantsBlue.Away.STACK_TO_BETWEEN_TO_JUNCTION.get())
+                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_TO_JUNCTION_TO_JUNCTION.get())
+                                .addTrajectory(AutoConstantsBlue.Away.W_JUNCTION_TO_LEFT.get())
                                 .addTrajectory(AutoConstantRed.Away.START_TO_W_JUNCTION.get())
                                 .addTrajectory(AutoConstantRed.Away.W_JUNCTION_TO_STACK.get())
                                 .addTrajectory(AutoConstantRed.Away.STACK_TO_W_JUNCTION.get())
