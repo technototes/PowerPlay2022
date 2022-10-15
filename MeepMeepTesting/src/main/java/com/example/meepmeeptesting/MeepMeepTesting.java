439c942b99a66eac703873ae76d4a92bbe728256
Yinn333 <hoda.ata18@gmail.com>
2022-10-14 17:56:16 -0700

    Doing the MeepMeep testing, and changing the coords in Red Constants

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,7 +1,6 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -15,15 +14,16 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .splineTo(new Vector2d(50, 50), Math.toRadians(90))
+                                .addTrajectory(AutoConstantRed.Away.START_TO_W_JUNCTION.get())
+                                .addTrajectory(AutoConstantRed.Away.W_JUNCTION_TO_STACK.get())
+                                .addTrajectory(AutoConstantRed.Away.STACK_TO_W_JUNCTION.get())
+                                .addTrajectory(AutoConstantRed.Away.W_JUNCTION_TO_STACK.get())
+                                .addTrajectory(AutoConstantRed.Away.STACK_TO_W_JUNCTION.get())
+                                .addTrajectory(AutoConstantRed.Away.W_JUNCTION_TO_STACK.get())
+                                .addTrajectory(AutoConstantRed.Away.STACK_TO_W_JUNCTION.get())
+                                .addTrajectory(AutoConstantRed.Away.W_JUNCTION_TO_STACK.get())
+                                .addTrajectory(AutoConstantRed.Away.STACK_TO_W_JUNCTION.get())
+                                .addTrajectory(AutoConstantRed.Away.W_JUNCTION_TO_PARK_LEFT.get())
                                 .build()
                 );
 
