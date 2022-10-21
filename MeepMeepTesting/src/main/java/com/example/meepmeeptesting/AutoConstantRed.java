b0a12c28837bc14971cd6c535d1121e6d22dd3a4
FTC16750 <technototes@outlook.com>
2022-10-21 16:14:42 -0700

    adding red constants to meep meep and some pathing

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutoConstantRed.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutoConstantRed.java
@@ -3,6 +3,16 @@ package com.example.meepmeeptesting;
 import static java.lang.Math.toRadians;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.acmerobotics.roadrunner.trajectory.constraints.AngularVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.MecanumVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.MinVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationConstraint;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class AutoConstantRed {
     public static class Home {
@@ -12,6 +22,26 @@ public class AutoConstantRed {
         public static Pose2d PARK_LEFT = new Pose2d(12, -36, toRadians(-90));
         public static Pose2d PARK_MIDDLE = new Pose2d(36, -36, toRadians(-90));
         public static Pose2d PARK_RIGHT = new Pose2d(60, -36, toRadians(180));
+
+        public static double MAX_VEL = 50;
+        public static double MAX_ACCEL = 40;
+        public static double MAX_ANG_VEL = Math.toRadians(180);
+        public static double MAX_ANG_ACCEL = Math.toRadians(120);
+        public static double TRACK_WIDTH = 9.5;
+
+        public static MinVelocityConstraint MIN_VEL = new MinVelocityConstraint(Arrays.asList(
+                new AngularVelocityConstraint(MAX_ANG_VEL),
+                new MecanumVelocityConstraint(MAX_VEL, TRACK_WIDTH)
+        ));
+        public static ProfileAccelerationConstraint PROF_ACCEL = new ProfileAccelerationConstraint(MAX_ACCEL);
+        public static Function<Pose2d, TrajectoryBuilder> function = pose -> new TrajectoryBuilder(pose, MIN_VEL, PROF_ACCEL);
+        public static Supplier<Trajectory>
+
+                START_TO_JUNCTION =
+                () -> function.apply(START)
+                        .splineTo(JUNCTION.vec(), JUNCTION.getHeading())
+                        .build();
+
     }
 
     public static class Away {
