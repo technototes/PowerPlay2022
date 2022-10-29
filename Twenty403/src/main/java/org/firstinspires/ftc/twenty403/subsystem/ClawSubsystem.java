39e75eff81672dd3100a797a6cb8e39eb3eca314
FTC16750 <technototes@outlook.com>
2022-10-29 10:13:33 -0700

    checks for if there is hardware on bot, if tries to run hardware not there, will crash

--- Twenty403/src/main/java/org/firstinspires/ftc/twenty403/subsystem/ClawSubsystem.java
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/subsystem/ClawSubsystem.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.twenty403.subsystem;
 
+import android.util.Log;
+
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -15,33 +17,61 @@ public class ClawSubsystem implements Subsystem {
     private Servo clawServo;
     private Servo flipperServo;
     private DistanceSensor sensor;
+    private Boolean isHardware;
 
     public ClawSubsystem(Servo claw, Servo flipper, DistanceSensor s) {
         clawServo = claw;
         flipperServo = flipper;
         sensor = s;
+        isHardware = true;
+    }
+
+    // Non-functional subsystem constructor
+    public ClawSubsystem() {
+        clawServo = null;
+        flipperServo = null;
+        sensor = null;
+        isHardware = false;
+
+    }
+
+    private static void log(String s) {
+        Log.d("Claw", s);
     }
 
     public void open() {
-        clawServo.setPosition(OPEN_SERVO_POSITION);
+        log("Open");
+        if (isHardware) {
+            clawServo.setPosition(OPEN_SERVO_POSITION);
+        }
     }
 
     public void close() {
-        clawServo.setPosition(CLOSE_SERVO_POSITION);
+        log("Close");
+        if (isHardware) {
+            clawServo.setPosition(CLOSE_SERVO_POSITION);
+        }
     }
 
     public void carry() {
+        log("Carry");
         close();
-        flipperServo.setPosition(CARRY_SERVO_POSITION);
+        if (isHardware) {
+            flipperServo.setPosition(CARRY_SERVO_POSITION);
+        }
     }
 
     public void release() {
-        flipperServo.setPosition(RELEASE_SERVO_POSITION);
+        log("Release");
+        if (isHardware) {
+            flipperServo.setPosition(RELEASE_SERVO_POSITION);
+        }
         open();
     }
 
     public boolean isConeClose() {
-        if (sensor.getDistance(DistanceUnit.CM) <= 4.0) {
+        log("isConeClose");
+        if (isHardware && sensor.getDistance(DistanceUnit.CM) <= 4.0) {
             return true;
         }
         return false;
