36c2ccf4ff5dd0ad40385a5d1381747d2799c5bf
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-11-02 20:36:31 -0700

    make EncoderDebugger configurable on what to print

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmode/EncoderDebugger.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmode/EncoderDebugger.java
@@ -17,6 +17,10 @@ public class EncoderDebugger extends CommandOpMode {
 
     ElapsedTime t;
 
+    public static boolean showEncoders = true;
+    public static boolean showIMUDetails = true;
+    public static boolean showIMUBasics = true;
+
     @Override
     public void uponInit() {
         drive = new SwerveDrivebaseSubsystem(hardwareMap);
@@ -38,34 +42,42 @@ public class EncoderDebugger extends CommandOpMode {
         double loopSeconds = t.seconds();
         t.reset();
         telemetry.addData("looptime",1/loopSeconds);
+
         //  TelemetryPacket packet = new TelemetryPacket();
         //  Canvas fieldOverlay = packet.fieldOverlay();
         //  DashboardUtil.drawRobot(fieldOverlay, poseEstimate);
         //  FtcDashboard.getInstance().sendTelemetryPacket(packet);
-        telemetry.addData("LeftFront-AbsoluteEncoder", drive.leftFrontModule.getModuleRotation());
-        telemetry.addData("LeftFront-MotorEncoder", drive.leftFrontModule.getWheelPosition());
 
-        telemetry.addData("LeftRear-AbsoluteEncoder", drive.leftRearModule.getModuleRotation());
-        telemetry.addData("LeftRear-MotorEncoder", drive.leftRearModule.getWheelPosition());
+        if (showEncoders) {
+            telemetry.addData("LeftFront-AbsoluteEncoder", drive.leftFrontModule.getModuleRotation());
+            telemetry.addData("LeftFront-MotorEncoder", drive.leftFrontModule.getWheelPosition());
 
-        telemetry.addData("RightFront-AbsoluteEncoder", drive.rightFrontModule.getModuleRotation());
-        telemetry.addData("RightFront-MotorEncoder", drive.rightFrontModule.getWheelPosition());
+            telemetry.addData("LeftRear-AbsoluteEncoder", drive.leftRearModule.getModuleRotation());
+            telemetry.addData("LeftRear-MotorEncoder", drive.leftRearModule.getWheelPosition());
 
-        telemetry.addData("RightRear-AbsoluteEncoder", drive.rightRearModule.getModuleRotation());
-        telemetry.addData("RightRear-MotorEncoder", drive.rightRearModule.getWheelPosition());
+            telemetry.addData("RightFront-AbsoluteEncoder", drive.rightFrontModule.getModuleRotation());
+            telemetry.addData("RightFront-MotorEncoder", drive.rightFrontModule.getWheelPosition());
 
-        telemetry.addData("IMU-AngularOrientation-FirstAngle", drive.imuAngularOrientation.firstAngle);
-        telemetry.addData("IMU-AngularOrientation-SecondAngle", drive.imuAngularOrientation.secondAngle);
-        telemetry.addData("IMU-AngularOrientation-ThirdAngle", drive.imuAngularOrientation.thirdAngle);
-        telemetry.addData("IMU-AngularVelocity-X", drive.imuAngularVelocity.xRotationRate);
-        telemetry.addData("IMU-AngularVelocity-Y", drive.imuAngularVelocity.yRotationRate);
-        telemetry.addData("IMU-AngularVelocity-Z", drive.imuAngularVelocity.zRotationRate);
+            telemetry.addData("RightRear-AbsoluteEncoder", drive.rightRearModule.getModuleRotation());
+            telemetry.addData("RightRear-MotorEncoder", drive.rightRearModule.getWheelPosition());
+        }
 
-        telemetry.addData("imuAngle", drive.imuAngle);
-        telemetry.addData("imuAngleVelocity", drive.imuAngleVelocity);
+        if (showIMUDetails) {
+            telemetry.addData("IMU-AngularOrientation-FirstAngle", drive.imuAngularOrientation.firstAngle);
+            telemetry.addData("IMU-AngularOrientation-SecondAngle", drive.imuAngularOrientation.secondAngle);
+            telemetry.addData("IMU-AngularOrientation-ThirdAngle", drive.imuAngularOrientation.thirdAngle);
+            telemetry.addData("IMU-AngularVelocity-X", drive.imuAngularVelocity.xRotationRate);
+            telemetry.addData("IMU-AngularVelocity-Y", drive.imuAngularVelocity.yRotationRate);
+            telemetry.addData("IMU-AngularVelocity-Z", drive.imuAngularVelocity.zRotationRate);
+        }
 
-        telemetry.addData("ExternalHeading", drive.getExternalHeading());
-        telemetry.addData("ExternalHeadingVelocity", drive.getExternalHeadingVelocity());
+        if (showIMUBasics) {
+            telemetry.addData("imuAngle", drive.imuAngle);
+            telemetry.addData("imuAngleVelocity", drive.imuAngleVelocity);
+
+            telemetry.addData("ExternalHeading", drive.getExternalHeading());
+            telemetry.addData("ExternalHeadingVelocity", drive.getExternalHeadingVelocity());
+        }
 
         telemetry.update();
     }
