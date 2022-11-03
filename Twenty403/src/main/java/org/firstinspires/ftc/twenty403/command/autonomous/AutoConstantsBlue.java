d2d387b9712a75feb4bd186630592f394dc09e64
FTC16750 <technototes@outlook.com>
2022-11-02 18:14:43 -0700

    gabriel finished a working path to score and park

--- Twenty403/src/main/java/org/firstinspires/ftc/twenty403/command/autonomous/AutoConstantsBlue.java
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/command/autonomous/AutoConstantsBlue.java
@@ -6,6 +6,7 @@ import java.util.function.Function;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.technototes.library.logger.LogConfig;
 import com.technototes.path.geometry.ConfigurablePose;
 import com.technototes.path.trajectorysequence.TrajectorySequence;
 import com.technototes.path.trajectorysequence.TrajectorySequenceBuilder;
@@ -18,8 +19,8 @@ public class AutoConstantsBlue {
         public static ConfigurablePose BETWEEN = new ConfigurablePose(-40, 12, toRadians(180));
         public static ConfigurablePose BETWEEN_2 = new ConfigurablePose(-36, 6, toRadians(90));
         public static ConfigurablePose BETWEEN_3 = new ConfigurablePose(-36, 36, toRadians(90));
-        public static ConfigurablePose LEFT = new ConfigurablePose(-12, 36, toRadians(-90));
-        public static ConfigurablePose MIDDLE = new ConfigurablePose(-36, 36, toRadians(90));
+        public static ConfigurablePose LEFT = new ConfigurablePose(12, -14, 4.71);
+        public static ConfigurablePose MIDDLE = new ConfigurablePose(36, -15, 4.71);
         public static ConfigurablePose RIGHT = new ConfigurablePose(-60, 36, toRadians(-90));
         // These have "home/away" modifiers, because we want to stay on "our side" during auto
         public static ConfigurablePose N_JUNCTION = new ConfigurablePose(18, -3, toRadians(-135));
@@ -30,6 +31,7 @@ public class AutoConstantsBlue {
         public static ConfigurablePose TEST_B = new ConfigurablePose(28, -4, toRadians(-135));
         public static ConfigurablePose BETWEEN_START_W_JUNCTION = new ConfigurablePose(36, -21, 2);
         public static ConfigurablePose BETWEEN_W_JUNCTION_STACK = new ConfigurablePose(36, -14, .4);
+        public static ConfigurablePose BETWEEN_STACK_W_JUNCTION = new ConfigurablePose(36, -14, 3);
         // These are 'trajectory pieces' which should be named like this:
         // {STARTING_POSITION}_TO_{ENDING_POSITION}
         public static final Function<Function<Pose2d, TrajectorySequenceBuilder>, TrajectorySequence>
@@ -48,6 +50,11 @@ public class AutoConstantsBlue {
                         b -> b.apply(W_JUNCTION.toPose())
                                 .lineToLinearHeading(BETWEEN_W_JUNCTION_STACK.toPose())
                                 .lineToLinearHeading(STACK.toPose())
+                                .build(),
+                STACK_TO_W_JUNCTION =
+                        b -> b.apply(STACK.toPose())
+                                .lineToLinearHeading(BETWEEN_STACK_W_JUNCTION.toPose())
+                                .lineToLinearHeading(W_JUNCTION.toPose())
                                 .build(),
                 START_TO_S_JUNCTION =
                         b -> b.apply(START.toPose())
@@ -77,16 +84,19 @@ public class AutoConstantsBlue {
                         b -> b.apply(BETWEEN.toPose())
                                 .lineToLinearHeading(E_JUNCTION.toPose())
                                 .build(),
-                START_TO_LEFT_PARK =
-                        b -> b.apply(START.toPose())
+                W_JUNCTION_TO_LEFT_PARK =
+                        b -> b.apply(W_JUNCTION.toPose())
+                                .lineToLinearHeading(BETWEEN_STACK_W_JUNCTION.toPose())
                                 .lineToLinearHeading(LEFT.toPose())
                                 .build(),
-                START_TO_MIDDLE_PARK =
-                        b -> b.apply(START.toPose())
+                W_JUNCTION_TO_MIDDLE_PARK =
+                        b -> b.apply(W_JUNCTION.toPose())
+                                .lineToLinearHeading(BETWEEN_STACK_W_JUNCTION.toPose())
                                 .lineToLinearHeading(MIDDLE.toPose())
                                 .build(),
-                START_TO_RIGHT_PARK =
-                        b -> b.apply(START.toPose())
+                W_JUNCTION_TO_RIGHT_PARK =
+                        b -> b.apply(W_JUNCTION.toPose())
+                                .lineToLinearHeading(BETWEEN_STACK_W_JUNCTION.toPose())
                                 .lineToLinearHeading(RIGHT.toPose())
                                 .build(),
 
