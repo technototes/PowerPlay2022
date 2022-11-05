323113127622b4d3b136cd988f0339a35e67d3b4
Yinn333 <hoda.ata18@gmail.com>
2022-11-05 12:40:25 -0700

    Hoda's stuff for claw & paths

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -9,64 +9,26 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(750);
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                .setDimensions(14, 14)
+//        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+//                .setDimensions(14, 14)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 9.5)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(AutoConstantsRed.Away.START)
-//                        .addTrajectory(AutoConstantsRed.Away.START_TO_W_JUNCTION.get())
-//                        .addTrajectory(AutoConstantsRed.Away.W_JUNCTION_TO_JUNCTION_TO_BETWEEN.get())
-//                        .addTrajectory(AutoConstantsRed.Away.BETWEEN_TO_STACK.get())
-//                        .addTrajectory(AutoConstantsRed.Away.STACK_TO_STACK_TO_BETWEEN.get())
-
-                                //cone1
-                                .addTrajectory(AutoConstantsRed.Home.START_TO_W_JUNCTION.get())
+//                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 9.5)
+//                .followTrajectorySequence(drive ->
+//                        drive.trajectorySequenceBuilder(AutoConstantsRed.Away.START)
+                //        .addTrajectory(AutoConstantsRed.Away.START_TO_LEFT_LOW.get()),
+                //        .addTrajectory()
 
 
-                                .addTrajectory(AutoConstantsRed.Home.W_JUNCTION_TO_BETWEEN.get())
-                                .addTrajectory(AutoConstantsRed.Home.BETWEEN_TO_STACK.get())
-                                .addTrajectory(AutoConstantsRed.Home.STACK_TO_BETWEEN.get())
-                                .addTrajectory(AutoConstantsRed.Home.BETWEEN_TO_W_JUNCTION.get())
-                                .addTrajectory(AutoConstantsRed.Home.W_JUNCTION_TO_PARK_MIDDLE.get())
+                //               .build()
 
+                // )
 
-//                                .addTrajectory(AutoConstantsBlue.Away.START_TO_W_JUNCTION.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.W_JUNCTION_TO_BETWEEN_TO_STACK.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_T0_STACK_TO_STACK.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.STACK_TO_BETWEEN_TO_JUNCTION.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_TO_JUNCTION_TO_JUNCTION.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.W_JUNCTION_TO_BETWEEN_TO_STACK.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_T0_STACK_TO_STACK.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.STACK_TO_BETWEEN_TO_JUNCTION.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_TO_JUNCTION_TO_JUNCTION.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.W_JUNCTION_TO_BETWEEN_TO_STACK.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_T0_STACK_TO_STACK.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.STACK_TO_BETWEEN_TO_JUNCTION.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_TO_JUNCTION_TO_JUNCTION.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.W_JUNCTION_TO_BETWEEN_TO_STACK.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_T0_STACK_TO_STACK.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.STACK_TO_BETWEEN_TO_JUNCTION.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.BETWEEN_TO_JUNCTION_TO_JUNCTION.get())
-//                                .addTrajectory(AutoConstantsBlue.Away.W_JUNCTION_TO_MIDDLE.get())
-// // >>>>>>> 5de6fbddabe59891c8bd45f0edb5ebd37bbfae50
-//                                .addTrajectory(AutoConstantsRed.Away.START_TO_W_JUNCTION.get())
-//                                .addTrajectory(AutoConstantsRed.Away.W_JUNCTION_TO_JUNCTION_TO_BETWEEN.get())
-//                                .addTrajectory(AutoConstantsRed.Away.BETWEEN_TO_STACK.get())
-//                                .addTrajectory(AutoConstantsRed.Away.STACK_TO_STACK_TO_BETWEEN.get())
-//
-
-
-
-                                .build()
-
-                );
 
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+//                .addEntity(myBot)
                 .start();
 
     }
---
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/command/autonomous/red_away/AutoRedAwayParkCommand.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.twenty403.command.autonomous.red_away;
+
+import com.technototes.library.command.SequentialCommandGroup;
+import com.technototes.library.subsystem.drivebase.DrivebaseSubsystem;
+
+import org.firstinspires.ftc.twenty403.subsystem.VisionSubsystem;
+
+public class AutoRedAwayParkCommand extends SequentialCommandGroup {
+    public AutoRedAwayParkCommand(DrivebaseSubsystem drivebaseSubsystem, VisionSubsystem visionSubsystem) {
+        super();
+    }
+}
