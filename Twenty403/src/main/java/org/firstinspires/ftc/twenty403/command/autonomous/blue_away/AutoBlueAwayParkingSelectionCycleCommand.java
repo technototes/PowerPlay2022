3c9666221f934d5127aed37ae604600d5ddf167b
FTC16750 <technototes@outlook.com>
2022-11-05 13:33:42 -0700

    Gabriel's pathing & command work

---
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/command/autonomous/blue_away/AutoBlueAwayParkingSelectionCycleCommand.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.twenty403.command.autonomous.blue_away;
+
+import android.util.Pair;
+
+import com.technototes.library.command.ChoiceCommand;
+
+import org.firstinspires.ftc.twenty403.subsystem.ClawSubsystem;
+import org.firstinspires.ftc.twenty403.subsystem.DrivebaseSubsystem;
+import org.firstinspires.ftc.twenty403.subsystem.LiftSubsystem;
+import org.firstinspires.ftc.twenty403.subsystem.VisionSubsystem;
+
+public class AutoBlueAwayParkingSelectionCycleCommand extends ChoiceCommand {
+    public AutoBlueAwayParkingSelectionCycleCommand(DrivebaseSubsystem drivebaseSubsystem, VisionSubsystem visionSubsystem, LiftSubsystem liftSubsystem, ClawSubsystem clawSubsystem) {
+        super(
+                new Pair<>(visionSubsystem.visionPipeline::left, new AutoBlueAwayCycleLeft(drivebaseSubsystem, clawSubsystem, liftSubsystem)),
+                new Pair<>(visionSubsystem.visionPipeline::middle, new AutoBlueAwayCycleMiddle(drivebaseSubsystem, clawSubsystem, liftSubsystem)),
+                new Pair<>(visionSubsystem.visionPipeline::right, new AutoBlueAwayCycleRight(drivebaseSubsystem, clawSubsystem, liftSubsystem))
+        );
+    }
+}
---
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/command/autonomous/blue_home/AutoBlueHomeParkingMiddle.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.twenty403.command.autonomous.blue_home;
+
+import org.firstinspires.ftc.twenty403.command.autonomous.AutoConstantsBlue;
+import org.firstinspires.ftc.twenty403.subsystem.DrivebaseSubsystem;
+
+import com.technototes.library.command.SequentialCommandGroup;
+import com.technototes.path.command.TrajectorySequenceCommand;
+
+public class AutoBlueHomeParkingMiddle extends SequentialCommandGroup {
+    public AutoBlueHomeParkingMiddle(DrivebaseSubsystem drivebaseSubsystem) {
+        super(new TrajectorySequenceCommand(drivebaseSubsystem, AutoConstantsBlue.Home.START_TO_MIDDLE_PARK));
+    }
+}
