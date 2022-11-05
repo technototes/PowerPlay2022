3ea8d867e95ee86309867a62e479e473c73887a1
FTC16750 <technototes@outlook.com>
2022-11-05 13:33:42 -0700

    Gabriel's pathing & command work

---
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/command/autonomous/blue_home/AutoBlueHomeCycleMiddle.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.twenty403.command.autonomous.blue_home;
+
+import com.technototes.library.command.SequentialCommandGroup;
+import com.technototes.path.command.TrajectorySequenceCommand;
+
+import org.firstinspires.ftc.twenty403.command.autonomous.AutoConstantsBlue;
+import org.firstinspires.ftc.twenty403.command.claw.ClawCloseCommand;
+import org.firstinspires.ftc.twenty403.command.claw.ClawOpenCommand;
+import org.firstinspires.ftc.twenty403.command.lift.LiftCollectCommand;
+import org.firstinspires.ftc.twenty403.command.lift.LiftHighJunctionCommand;
+import org.firstinspires.ftc.twenty403.subsystem.ClawSubsystem;
+import org.firstinspires.ftc.twenty403.subsystem.DrivebaseSubsystem;
+import org.firstinspires.ftc.twenty403.subsystem.LiftSubsystem;
+
+public class AutoBlueHomeCycleMiddle extends SequentialCommandGroup {
+    public AutoBlueHomeCycleMiddle(DrivebaseSubsystem drivebaseSubsystem, ClawSubsystem clawSubsystem, LiftSubsystem liftSubsystem) {
+        super(
+                new TrajectorySequenceCommand(drivebaseSubsystem, AutoConstantsBlue.Home.START_TO_W_JUNCTION).alongWith((new LiftHighJunctionCommand(liftSubsystem)),
+                        new ClawOpenCommand(clawSubsystem),
+                        new TrajectorySequenceCommand(drivebaseSubsystem, AutoConstantsBlue.Home.W_JUNCTION_TO_STACK).alongWith(new LiftCollectCommand(liftSubsystem)),
+                        new ClawCloseCommand(clawSubsystem),
+                        new TrajectorySequenceCommand(drivebaseSubsystem, AutoConstantsBlue.Home.STACK_TO_W_JUNCTION).alongWith(new LiftHighJunctionCommand(liftSubsystem)),
+                        new ClawOpenCommand(clawSubsystem),
+                        new TrajectorySequenceCommand(drivebaseSubsystem, AutoConstantsBlue.Home.W_JUNCTION_TO_MIDDLE_PARK).alongWith(new LiftCollectCommand(liftSubsystem))
+                ));
+    }
+}
