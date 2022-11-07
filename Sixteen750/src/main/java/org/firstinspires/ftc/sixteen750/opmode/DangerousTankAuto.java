0227a5fb66e735d4e4fbd3fd578f18a9656f7a1e
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-11-06 16:44:58 -0800

    WE PARKED!

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmode/DangerousTankAuto.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmode/DangerousTankAuto.java
@@ -8,14 +8,15 @@ import org.firstinspires.ftc.sixteen750.subsystem.TankDrivebaseSubsystem;
 
 @Autonomous(name = "Move Forward")
 public class DangerousTankAuto extends LinearOpMode {
+    public static int DURATION_MS = 300;
     @Override
     public void runOpMode() throws InterruptedException {
         TankDrivebaseSubsystem drive = new TankDrivebaseSubsystem(hardwareMap);
         waitForStart();
         if (isStopRequested()) return;
         ElapsedTime time = new ElapsedTime();
-        while (!isStopRequested() && opModeIsActive() && time.milliseconds() < 1000) {
-            drive.setMotorPowers(0.5, 0.5);
+        while (!isStopRequested() && opModeIsActive() && time.milliseconds() < DURATION_MS) {
+            drive.setMotorPowers(0.5, -0.5);
             telemetry.addData("Loop Count", time.milliseconds());
             telemetry.update();
         }
