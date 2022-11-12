5ca127a87327738d67c2c9c35ac53e89fa1ffead
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-11-11 23:53:31 -0800

    rename constants and method names in LiftSubsystem

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/subsystem/LiftSubsystem.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/subsystem/LiftSubsystem.java
@@ -13,9 +13,14 @@ import com.technototes.library.logger.Loggable;
 import com.technototes.library.subsystem.Subsystem;
 
 @Config
+@SuppressWarnings("unused")
 public class LiftSubsystem implements Subsystem, Supplier<Double>, Loggable {
+    // Assuming the 0 position for both lift motor might be different
+    // The LiftSubsystem should be able to any of the motor combination
     // TODO: THESE VALUES ARE ALL WRONG! THEY NEED TO BE SET TO THE RIGHT VALUES!!!!
     public static double TICKS_INCH = 750; // WRONG,
+    public static double L_INTAKE;
+    public static double R_INTAKE;
     public static double L_GROUND_JUNCTION = 0.5 * TICKS_INCH; // WRONG
     public static double R_GROUND_JUNCTION = 0.5 * TICKS_INCH; // WRONG
     public static double L_LOW_JUNCTION = 12 * TICKS_INCH; // WRONG
@@ -24,19 +29,19 @@ public class LiftSubsystem implements Subsystem, Supplier<Double>, Loggable {
     public static double R_MEDIUM_JUNCTION = 24 * TICKS_INCH; // WRONG
     public static double L_HIGH_JUNCTION = 36 * TICKS_INCH; // WRONG
     public static double R_HIGH_JUNCTION = 36 * TICKS_INCH; // WRONG
-    public static double MAX_HEIGHT = 38 * TICKS_INCH; // WRONG
-    public static double LEFT_ABSOLUTE_MAX_HEIGHT = 1651;
-    public static double RIGHT_ABSOLUTE_MAX_HEIGHT = 1651; // COPIED FROM LEFT
-    public static double MIN_HEIGHT = 0;
-    public static double LEFT_ABSOLUTE_MIN_HEIGHT = 15;
-    public static double RIGHT_ABSOLUTE_MIN_HEIGHT = 15; // COPIED FROM LEFT
-    public static double MAX_DISTANCE_FOR_FULLPOWER = 8 * TICKS_INCH; // WRONG
-    public static double DEAD_ZONE = .25 * TICKS_INCH; // WRONG
-    public static double MAX_MOTOR_SPEED = 0.8;
-    public static double MIN_MOTOR_SPEED = -0.4; // Gravity
+    public static double L_ABSOLUTE_MIN_HEIGHT = 15;
+    public static double L_ABSOLUTE_MAX_HEIGHT = 1651;
+    public static double R_ABSOLUTE_MIN_HEIGHT = 15; // COPIED FROM LEFT
+    public static double R_ABSOLUTE_MAX_HEIGHT = 1651; // COPIED FROM LEFT
+    public static double L_MAX_MOTOR_SPEED = 0.8; // Unverified
+    public static double L_MIN_MOTOR_SPEED = -0.4; // Unverified, Gravity
+    public static double R_MAX_MOTOR_SPEED = 0.8; // Unverified
+    public static double R_MIN_MOTOR_SPEED = -0.4; // Unverified, Gravity
     public static double L_INCREMENTAL_MOVE = 10;
     public static double R_INCREMENTAL_MOVE = 10;
-    public static PIDCoefficients PID = new PIDCoefficients(0.008, 0, 0.0005);
+    public static PIDCoefficients L_PID = new PIDCoefficients(0.008, 0, 0.0005); // Unverified
+    public static PIDCoefficients R_PID = new PIDCoefficients(0.008, 0, 0.0005); // Unverified
+    public static double TOLERANCE_ZONE = 2; // Unverified
 
     private EncodedMotor<DcMotorEx> leftMotor;
     private PIDFController leftPidController;
@@ -49,7 +54,7 @@ public class LiftSubsystem implements Subsystem, Supplier<Double>, Loggable {
     public LiftSubsystem(EncodedMotor<DcMotorEx> leftM, EncodedMotor<DcMotorEx> rightM) {
         if (leftM != null) {
             this.leftMotor = leftM;
-            this.leftPidController = new PIDFController(PID, 0, 0, 0, (x, y) -> 0.1);
+            this.leftPidController = new PIDFController(L_PID, 0, 0, 0, (x, y) -> 0.1);
             this.isLeftConnected = true;
         } else {
             this.isLeftConnected = false;
@@ -57,7 +62,7 @@ public class LiftSubsystem implements Subsystem, Supplier<Double>, Loggable {
 
         if (rightM != null) {
             this.rightMotor = rightM;
-            this.rightPidController = new PIDFController(PID, 0, 0, 0, (x, y) -> 0.1);
+            this.rightPidController = new PIDFController(R_PID, 0, 0, 0, (x, y) -> 0.1);
             this.isRightConnected = true;
         } else {
             this.isRightConnected = false;
@@ -66,23 +71,91 @@ public class LiftSubsystem implements Subsystem, Supplier<Double>, Loggable {
 
     private void setTargetPosition(double lPos, double rPos) {
         if (isLeftConnected) {
-            leftPidController.setTargetPosition(Range.clip(lPos, LEFT_ABSOLUTE_MIN_HEIGHT, LEFT_ABSOLUTE_MAX_HEIGHT));
+            leftPidController.setTargetPosition(Range.clip(lPos, L_ABSOLUTE_MIN_HEIGHT, L_ABSOLUTE_MAX_HEIGHT));
         }
         if (isRightConnected) {
-            rightPidController.setTargetPosition(
-                    Range.clip(rPos, RIGHT_ABSOLUTE_MIN_HEIGHT, RIGHT_ABSOLUTE_MAX_HEIGHT));
+            rightPidController.setTargetPosition(Range.clip(rPos, R_ABSOLUTE_MIN_HEIGHT, R_ABSOLUTE_MAX_HEIGHT));
         }
     }
 
-    public void setTop() {
-        // null check will be done in setTargetPosition()
-        this.setTargetPosition(LEFT_ABSOLUTE_MAX_HEIGHT, RIGHT_ABSOLUTE_MAX_HEIGHT);
+    private void setTargetPositionWithLogging(double lPos, double rPos) {
+        setTargetPosition(lPos, rPos);
+        if (isLeftConnected) {
+            lpAndActual = String.format("%d (%d)", (int) lPos, leftMotor.get().intValue());
+        }
+        if (isRightConnected) {
+            rpAndActual = String.format("%d (%d)", (int) rPos, rightMotor.get().intValue());
+        }
     }
 
-    public void setBottom() {
-        // null check will be done in setTargetPosition()
-        this.setTargetPosition(LEFT_ABSOLUTE_MIN_HEIGHT, RIGHT_ABSOLUTE_MIN_HEIGHT);
+    // This is run for every loop (Feature of the TechnoLib Subsystem!)
+    // So you can just call "setTop" in a command and it will get there "as soon as it can"
+    @Override
+    public void periodic() {
+        if (isLeftConnected) {
+            double leftTargetSpeed = leftPidController.update(getLeftPos());
+            double leftClippedSpeed = Range.clip(leftTargetSpeed, L_MIN_MOTOR_SPEED, L_MAX_MOTOR_SPEED);
+            leftMotor.setSpeed(leftClippedSpeed);
+        }
+        if (isRightConnected) {
+            double rTargetSpeed = rightPidController.update(getRightPos());
+            double rClippedSpeed = Range.clip(rTargetSpeed, L_MIN_MOTOR_SPEED, L_MAX_MOTOR_SPEED);
+            rightMotor.setSpeed(rClippedSpeed);
+        }
     }
+
+    public double leftDelta() {
+        return leftPidController.getTargetPosition() - leftMotor.getDevice().getCurrentPosition();
+    }
+
+    public double rightDelta() {
+        return rightPidController.getTargetPosition() - rightMotor.getDevice().getCurrentPosition();
+    }
+
+    public boolean isLeftAtTarget() {
+        return Math.abs(leftDelta()) < TOLERANCE_ZONE;
+    }
+
+    public boolean isRightAtTarget() {
+        return Math.abs(rightDelta()) < TOLERANCE_ZONE;
+    }
+
+    public double getLeftPos() {
+        if (isLeftConnected) {
+            return leftMotor.getEncoder().getPosition();
+        }
+        return 0.0;
+    }
+
+    public double getRightPos() {
+        if (isRightConnected) {
+            // Invert the sign on this one to make it look like it's rotating the same way...
+            return -rightMotor.getEncoder().getPosition();
+        }
+        return 0.0;
+    }
+
+    @Override
+    public Double get() {
+        // Not sure about this one
+        if (isLeftConnected) {
+            return leftPidController.getTargetPosition();
+        }
+        else if (isRightConnected) {
+            return rightPidController.getTargetPosition();
+        }
+        else {
+            return 0.0;
+        }
+    }
+
+    // TODO: enable as needed
+    // @Log(name = "lEncMotor Pos (Actual)")
+    public volatile String lpAndActual = "(unknown)";
+
+    // TODO: enable as needed
+    // @Log(name = "rEncMotor Pos (Actual)")
+    public volatile String rpAndActual = "(unknown)";
 
     public void stop() {
         // By resetting the pidController, it stops the periodic function from making changes
@@ -104,90 +177,32 @@ public class LiftSubsystem implements Subsystem, Supplier<Double>, Loggable {
         }
     }
 
-    private void setTargetPositionWithLogging(double lVal, double rVal) {
-        setTargetPosition(lVal, rVal);
-        if (isLeftConnected) {
-            lpAndActual = String.format("%d (%d)", (int) lVal, leftMotor.get().intValue());
-        }
-        if (isRightConnected) {
-            rpAndActual = String.format("%d (%d)", (int) rVal, rightMotor.get().intValue());
-        }
+    public void gotoTop() {
+        // null check will be done in setTargetPosition()
+        this.setTargetPosition(L_ABSOLUTE_MAX_HEIGHT, R_ABSOLUTE_MAX_HEIGHT);
     }
 
-    // This is run for every loop (Feature of the TechnoLib Subsystem!)
-    // So you can just call "setTop" in a command and it will get there "as soon as it can"
-    @Override
-    public void periodic() {
-        if (isLeftConnected) {
-            double leftTargetSpeed = leftPidController.update(getLeftPos());
-            double leftClippedSpeed = Range.clip(leftTargetSpeed, MIN_MOTOR_SPEED, MAX_MOTOR_SPEED);
-            leftMotor.setSpeed(leftClippedSpeed);
-        }
-        if (isRightConnected) {
-            double rTargetSpeed = rightPidController.update(getRightPos());
-            double rClippedSpeed = Range.clip(rTargetSpeed, MIN_MOTOR_SPEED, MAX_MOTOR_SPEED);
-            rightMotor.setSpeed(rClippedSpeed);
-            // For logging purposes, I'm also doing this, to ensure that both values are updated
-        }
-        //        double rightTarget = 0;
-        //        if (!singleMotor) {
-        //            rightTarget = rightPidController.getTargetPosition();
-        //        }
-        //        setTargetPositionWithLogging(leftPidController.getTargetPosition(), rightTarget);
+    public void gotoBottom() {
+        // null check will be done in setTargetPosition()
+        this.setTargetPosition(L_ABSOLUTE_MIN_HEIGHT, R_ABSOLUTE_MIN_HEIGHT);
     }
 
-    //    public double delta() {
-    //        return leftPidController.getTargetPosition() - leftMotor.getDevice().getCurrentPosition();
-    //        return rightPidController.getTargetPosition() - rightMotor.getDevice().getCurrentPosition();
-    //    }
-
-    //    public boolean isAtTarget() {
-    //        return Math.abs(delta()) < DEAD_ZONE;
-    //    }
-
-    // TODO: enable as needed
-    //    @Log(name = "lEncMotor Pos (Actual)")
-    public volatile String lpAndActual = "(unknown)";
-
-    // TODO: enable as needed
-    //    @Log(name = "rEncMotor Pos (Actual)")
-    public volatile String rpAndActual = "(unknown)";
-
-    @Override
-    public Double get() {
-        // Not sure about this one
-        if (isLeftConnected) {
-            return leftPidController.getTargetPosition();
-        }
-        return 0.0;
-        //        return rightPidController.getTargetPosition();
-    }
-
-    //    private boolean closeEnough(double currentPostion, double targetPostion) {
-    //        double error = currentPostion - targetPostion;
-    //        if (error <= DEAD_ZONE && error >= -DEAD_ZONE) {
-    //            return true;
-    //        } else {
-    //            return false;
-    //        }
-    //    }
-
-    public void highPole() {
+    public void gotoHighPole() {
         // null check will be done in setTargetPosition()
         setTargetPositionWithLogging(L_HIGH_JUNCTION, R_HIGH_JUNCTION);
     }
 
-    public void midPole() {
+    public void gotoMidPole() {
         // null check will be done in setTargetPosition()
         setTargetPositionWithLogging(L_MEDIUM_JUNCTION, R_MEDIUM_JUNCTION);
     }
 
-    public void lowPole() {
+    public void gotoLowPole() {
         // null check will be done in setTargetPosition()
         setTargetPositionWithLogging(L_LOW_JUNCTION, R_LOW_JUNCTION);
     }
 
-    public void groundJunction() {
+    public void gotoGroundJunction() {
         // null check will be done in setTargetPosition()
         setTargetPositionWithLogging(L_GROUND_JUNCTION, R_GROUND_JUNCTION);
     }
@@ -196,8 +211,7 @@ public class LiftSubsystem implements Subsystem, Supplier<Double>, Loggable {
         if (isLeftConnected) {
             // maybe getCurrentPosition instead of getTargetPosition
             double rightTargetPosition = leftPidController.getTargetPosition();
-            setTargetPositionWithLogging(
-                    rightTargetPosition + L_INCREMENTAL_MOVE, rightTargetPosition + R_INCREMENTAL_MOVE);
+            setTargetPositionWithLogging(rightTargetPosition + L_INCREMENTAL_MOVE, rightTargetPosition + R_INCREMENTAL_MOVE);
         }
     }
 
@@ -205,23 +219,7 @@ public class LiftSubsystem implements Subsystem, Supplier<Double>, Loggable {
         if (isRightConnected) {
             // maybe getCurrentPosition instead of getTargetPosition
             double leftTargetPosition = leftPidController.getTargetPosition();
-            setTargetPositionWithLogging(
-                    leftTargetPosition - L_INCREMENTAL_MOVE, leftTargetPosition - R_INCREMENTAL_MOVE);
+            setTargetPositionWithLogging(leftTargetPosition - L_INCREMENTAL_MOVE, leftTargetPosition - R_INCREMENTAL_MOVE);
         }
-    }
-
-    public double getLeftPos() {
-        if (isLeftConnected) {
-            return leftMotor.getEncoder().getPosition();
-        }
-        return 0.0;
-    }
-
-    public double getRightPos() {
-        if (isRightConnected) {
-            // Invert the sign on this one to make it look like it's rotating the same way...
-            return -rightMotor.getEncoder().getPosition();
-        }
-        return 0.0;
     }
 }
