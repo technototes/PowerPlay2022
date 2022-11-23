e7f3527ac8867a2f9f77f6cdc6d72d1f6fe29fb2
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-11-22 17:26:49 -0800

    finish MecanumInfinityStraightTest

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmode/diagnosis/MecanumInfinityStraightTest.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/opmode/diagnosis/MecanumInfinityStraightTest.java
@@ -1,16 +1,23 @@
 package org.firstinspires.ftc.sixteen750.opmode.diagnosis;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.technototes.path.trajectorysequence.TrajectorySequence;
 
 import org.firstinspires.ftc.sixteen750.Hardware;
 import org.firstinspires.ftc.sixteen750.Robot;
 
+@Config
+@Autonomous(group = "Test-Path")
+@SuppressWarnings("unused")
 public class MecanumInfinityStraightTest extends LinearOpMode {
     public static double DISTANCE = 20; // in
     int loopCount = 0;
+    Pose2d lastPoseEstimate;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -18,12 +25,20 @@ public class MecanumInfinityStraightTest extends LinearOpMode {
         Hardware hardware = new Hardware(hardwareMap, true, false, false, false);
         Robot robot = new Robot(hardware, true, false, false, false);
 
+        TrajectorySequence backandFourthTrajectory = robot.mecanumDriveSubsystem.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                .forward(DISTANCE)
+                .back(DISTANCE)
+                .build();
+
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (!isStopRequested() && opModeIsActive()) {
+            robot.mecanumDriveSubsystem.followTrajectorySequence(backandFourthTrajectory);
+            lastPoseEstimate = robot.mecanumDriveSubsystem.getPoseEstimate();
             telemetry.addData("Loop Count", 0);
+            telemetry.addData("Last Pose Estimate", lastPoseEstimate);
             telemetry.update();
             loopCount++;
         }
