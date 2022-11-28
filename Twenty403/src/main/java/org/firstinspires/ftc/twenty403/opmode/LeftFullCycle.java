59c88abe4e20c392360c3b72d767d3c17a36e735
Yinn333 <hoda.ata18@gmail.com>
2022-11-27 16:18:38 -0800

    worked on Left Auto

--- Twenty403/src/main/java/org/firstinspires/ftc/twenty403/opmode/LeftFullCycle.java
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/opmode/LeftFullCycle.java
@@ -7,6 +7,7 @@ import org.firstinspires.ftc.twenty403.command.VisionCommand;
 import org.firstinspires.ftc.twenty403.command.autonomous.AutoConstantsBlue;
 import org.firstinspires.ftc.twenty403.command.autonomous.StartingPosition;
 import org.firstinspires.ftc.twenty403.command.autonomous.blue_away.AutoBlueAwayParkingSelectionFullCycleCommand;
+import org.firstinspires.ftc.twenty403.command.claw.ClawCloseCommand;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
@@ -29,6 +30,7 @@ public class LeftFullCycle extends CommandOpMode {
         CommandScheduler.getInstance()
                 .scheduleForState(
                         new SequentialCommandGroup(
+                                //new ClawCloseCommand(robot.clawSubsystem),
                                 new AutoBlueAwayParkingSelectionFullCycleCommand(
                                         robot.visionSystem,
                                         robot.drivebaseSubsystem,
@@ -36,8 +38,12 @@ public class LeftFullCycle extends CommandOpMode {
                                         robot.liftSubsystem),
                                 CommandScheduler.getInstance()::terminateOpMode),
                         CommandOpMode.OpModeState.RUN);
+        //Claw close on Init doesn't work yet
         if (Robot.RobotConstant.CAMERA_CONNECTED) {
-            CommandScheduler.getInstance().scheduleInit(new VisionCommand(robot.visionSystem));
+            CommandScheduler.getInstance().scheduleInit(new VisionCommand(robot.visionSystem).alongWith(new ClawCloseCommand(robot.clawSubsystem)));
+        }
+        else {
+            CommandScheduler.getInstance().scheduleInit(new ClawCloseCommand(robot.clawSubsystem));
         }
     }
 }
