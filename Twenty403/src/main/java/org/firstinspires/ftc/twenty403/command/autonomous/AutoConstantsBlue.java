8345b65e363e7c82812836b757e6791be7720f35
FTC16750 <technototes@outlook.com>
2022-12-01 18:54:21 -0800

    right side auto constants

--- Twenty403/src/main/java/org/firstinspires/ftc/twenty403/command/autonomous/AutoConstantsBlue.java
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/command/autonomous/AutoConstantsBlue.java
@@ -20,15 +20,15 @@ public class AutoConstantsBlue {
         public static ConfigurablePose LEFT_MOVE = new ConfigurablePose(-24, 0, toRadians(90));
         public static ConfigurablePose RIGHT_MOVE = new ConfigurablePose(24, 0, toRadians(90));
 
-        public static ConfigurablePose STACK = new ConfigurablePose(70, -14, toRadians(0));
+        public static ConfigurablePose STACK = new ConfigurablePose(71, -17, toRadians(0));
         public static ConfigurablePose LEFT = new ConfigurablePose(18, -18, toRadians(90));
         public static ConfigurablePose MIDDLE = new ConfigurablePose(36, -16, toRadians(90));
         public static ConfigurablePose RIGHT = new ConfigurablePose(60, -17, toRadians(90));
-        public static ConfigurablePose W_JUNCTION = new ConfigurablePose(31, -10, 2);
+        public static ConfigurablePose W_JUNCTION = new ConfigurablePose(31, -11, 2);
 
         //public static ConfigurablePose BETWEEN_START_W_jUNCTION_ONE = new ConfigurablePose(40, -48, toRadians(180));
         public static ConfigurablePose BETWEEN_START_W_JUNCTION_TWO = new ConfigurablePose(42, -12, 3.8);
-        public static ConfigurablePose BETWEEN_W_JUNCTION_STACK = new ConfigurablePose(43, -23, 1.6);
+        public static ConfigurablePose BETWEEN_W_JUNCTION_STACK = new ConfigurablePose(43, -20, 1.5);
         public static ConfigurablePose BETWEEN_STACK_W_JUNCTION = new ConfigurablePose(40, -14, 2.4);
         public static ConfigurablePose BETWEEN_START_LEFT = new ConfigurablePose(15, -60, toRadians(90));
         public static ConfigurablePose BETWEEN_START_RIGHT = new ConfigurablePose(60, -60, toRadians(90));
