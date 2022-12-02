a445f3d5d75d05ac3eca0191f60ebf4b3f6ccd9f
Kevin Frei <kevinfrei@hotmail.com>
2022-12-02 08:42:55 -0800

    Fixed bug from Tio's work Thursday

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/command/lift/LiftMoveDownOverrideCommand.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/command/lift/LiftMoveDownOverrideCommand.java
@@ -14,6 +14,6 @@ public class LiftMoveDownOverrideCommand implements Command {
 
     @Override
     public void execute() {
-        liftSubsystem.moveUpOVERRIDE();
+        liftSubsystem.moveDownOVERRIDE();
     }
 }
