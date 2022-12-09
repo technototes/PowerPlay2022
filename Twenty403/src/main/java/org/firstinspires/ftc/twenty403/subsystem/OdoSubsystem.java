d70c785f307585b41cdcfecdc73e4915c7a8ec37
Kevin Frei <kevinfrei@hotmail.com>
2022-12-09 14:49:14 -0800

    Added the 'periodic' function to read the sensors

--- Twenty403/src/main/java/org/firstinspires/ftc/twenty403/subsystem/OdoSubsystem.java
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/subsystem/OdoSubsystem.java
@@ -3,9 +3,11 @@ package org.firstinspires.ftc.twenty403.subsystem;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.technototes.library.subsystem.Subsystem;
+
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-public class OdoSubsystem {
+public class OdoSubsystem implements Subsystem {
 
     DistanceSensor dLeft;
     DistanceSensor dRight;
@@ -13,6 +15,12 @@ public class OdoSubsystem {
     RevColorSensorV3 cLeft;
     ColorRangeSensor cMiddle;
     RevColorSensorV3 cRight;
+
+    double leftDistance;
+    double rightDistance;
+    int leftColor;
+    int middleColor;
+    int rightColor;
 
     public OdoSubsystem(
         DistanceSensor dl,
@@ -28,14 +36,24 @@ public class OdoSubsystem {
         cRight = cr;
     }
 
-    double CheckDistance() {
+    public double WallDistance() {
         // if left and right distance sensors measure the same distance
-        if (dLeft.getDistance(DistanceUnit.CM) == dRight.getDistance(DistanceUnit.CM)) {
+        if (leftDistance == rightDistance) {
             //
         } else if (dLeft.getDistance(DistanceUnit.CM) < dRight.getDistance(DistanceUnit.CM)) {
             //
         } else if (dLeft.getDistance(DistanceUnit.CM) > dRight.getDistance(DistanceUnit.CM)) {
             //
         }
+        return 1234.5;
+    }
+
+    @Override public void periodic() {
+        // Read the sensors and squirrel away the value
+        leftDistance = dLeft.getDistance(DistanceUnit.CM);
+        rightDistance = dRight.getDistance(DistanceUnit.CM);
+        leftColor = cLeft.argb();
+        middleColor = cMiddle.argb();
+        rightColor = cRight.argb();
     }
 }
