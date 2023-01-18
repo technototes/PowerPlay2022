bb324a98acd397456d22250cd6c7b91fba3e07d4
FTC16750 <technototes@outlook.com>
2023-01-17 17:18:46 -0800

    - Fix the Module - Replace setMotorPower to setMotorVelocity - replaced the while loop with isFinished

--- Swerveteen750/src/main/java/org/firstinspires/ftc/swerveteen750/command/drive/LRDriveSetDistanceCommand.java
+++ Swerveteen750/src/main/java/org/firstinspires/ftc/swerveteen750/command/drive/LRDriveSetDistanceCommand.java
@@ -3,23 +3,33 @@ package org.firstinspires.ftc.swerveteen750.command.drive;
 import com.technototes.library.command.Command;
 
 import org.firstinspires.ftc.swerveteen750.subsystem.drive.ConfigurableSwerveDriveSubsystem;
+import org.firstinspires.ftc.swerveteen750.swerve_util.swerve_module.AnotherSwerveModule;
 
 public class LRDriveSetDistanceCommand implements Command {
-    private ConfigurableSwerveDriveSubsystem subsystem;
+
     private double ticksPerdistance;
     private double startingEncoderValue;
+    private AnotherSwerveModule module;
 
-    public LRDriveSetDistanceCommand(ConfigurableSwerveDriveSubsystem s, double d) {
-        subsystem = s;
+
+    public LRDriveSetDistanceCommand(AnotherSwerveModule m, double d) {
+        module = m;
         ticksPerdistance = ConfigurableSwerveDriveSubsystem.getTicksFromInches(d);
-        startingEncoderValue = s.leftFrontModule.getWheelPosition();
+        startingEncoderValue = module.getWheelPosition();
     }
 
     @Override
     public void execute() {
-        while (subsystem.leftRearModule.getWheelPosition() - startingEncoderValue < ticksPerdistance) {
-            subsystem.leftRearModule.setMotorPower(1);
+        module.setMotorVelocity(1);
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (module.getWheelPosition() - startingEncoderValue < ticksPerdistance) {
+            return false;
+        } else {
+            module.setMotorVelocity(0);
+            return true;
         }
-        subsystem.leftRearModule.setMotorPower(0);
     }
 }
