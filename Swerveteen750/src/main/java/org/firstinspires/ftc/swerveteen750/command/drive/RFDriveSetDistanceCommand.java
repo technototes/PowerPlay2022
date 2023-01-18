a6aa248906ce6e51409b8d606a7f5f8755f9e872
FTC16750 <technototes@outlook.com>
2023-01-17 17:18:36 -0800

    Fixed all errors with RFDriveSetDistanceCommand and RRDriveSetDistanceCommand such as replacing the subsystem with a module, replacing setMotorPower with setMotorVelocity, and adding isFinished.

--- Swerveteen750/src/main/java/org/firstinspires/ftc/swerveteen750/command/drive/RFDriveSetDistanceCommand.java
+++ Swerveteen750/src/main/java/org/firstinspires/ftc/swerveteen750/command/drive/RFDriveSetDistanceCommand.java
@@ -3,23 +3,32 @@ package org.firstinspires.ftc.swerveteen750.command.drive;
 import com.technototes.library.command.Command;
 
 import org.firstinspires.ftc.swerveteen750.subsystem.drive.ConfigurableSwerveDriveSubsystem;
+import org.firstinspires.ftc.swerveteen750.swerve_util.swerve_module.AnotherSwerveModule;
 
 public class RFDriveSetDistanceCommand implements Command {
-    private ConfigurableSwerveDriveSubsystem subsystem;
+
     private double ticksPerdistance;
     private double startingEncoderValue;
+    private AnotherSwerveModule module;
 
-    public RFDriveSetDistanceCommand(ConfigurableSwerveDriveSubsystem s, double d) {
-        subsystem = s;
+    public RFDriveSetDistanceCommand(AnotherSwerveModule m, double d) {
+        module = m;
         ticksPerdistance = ConfigurableSwerveDriveSubsystem.getTicksFromInches(d);
-        startingEncoderValue = s.leftFrontModule.getWheelPosition();
+        startingEncoderValue = module.getWheelPosition();
     }
 
     @Override
     public void execute() {
-        while (subsystem.rightFrontModule.getWheelPosition() - startingEncoderValue < ticksPerdistance) {
-            subsystem.rightFrontModule.setMotorPower(1);
+        module.setMotorVelocity(1);
+    }
+
+    public boolean isFinished() {
+        if (module.getWheelPosition() - startingEncoderValue < ticksPerdistance) {
+          return false;
         }
-        subsystem.rightFrontModule.setMotorPower(0);
+        else {
+            module.setMotorVelocity(0);
+            return true;
+        }
     }
 }
